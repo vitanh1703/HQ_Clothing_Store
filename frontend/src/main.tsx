d0b9eb7fe1b36f9b67eb988752b1ae2b703fff83
vitanh1703 <diema448@gmail.com>
2026-04-03 17:53:07 +0700

    update login google auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,8 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* ID này bạn lấy từ Google Cloud Console, tạm thời để demo */}
-    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="249381559845-1s30c3kjmaeic2v35il5vjqir9930pq2.apps.googleusercontent.com">
       <BrowserRouter>
         <App />
       </BrowserRouter>

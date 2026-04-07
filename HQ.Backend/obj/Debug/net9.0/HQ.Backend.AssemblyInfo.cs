35654532c2ea43301d856f5dda0a779c1a7c10e8
Nguyen Thi Hao <hao1512005@gmail.com>
2026-04-07 23:52:18 +0700

    update select color

--- HQ.Backend/obj/Debug/net9.0/HQ.Backend.AssemblyInfo.cs
+++ HQ.Backend/obj/Debug/net9.0/HQ.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HQ.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f7fbfcbf303c30e1119a382eda61b86ec6220b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+513b5174b6244c9e222cfd7a429a222225b02fe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HQ.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HQ.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
